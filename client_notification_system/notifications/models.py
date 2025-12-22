b4dcfe1c465ec7d83128f6d75f67f9c1b165b3d7
skaduteye <skaduteye@gmail.com>
2025-12-22 00:18:32 +0000

    add sms template model

--- client_notification_system/notifications/models.py
+++ client_notification_system/notifications/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class SMSTemplate(models.Model):
+    """Reusable SMS template for a user"""
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='sms_templates'
+    )
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
