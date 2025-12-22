3a0ee6d8eb08ce462f19f985ca649cde6bffe683
skaduteye <skaduteye@gmail.com>
2025-12-22 00:16:31 +0000

    add client model

--- client_notification_system/clients/models.py
+++ client_notification_system/clients/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Client(models.Model):
+    """Client contact that belongs to a user"""
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='clients'
+    )
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.name
